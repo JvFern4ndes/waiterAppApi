93815577f28e624f4812ac9fc411e9cf93f4a5d9
João Victor <jvfernandes.contato@gmail.com>
2023-01-18 20:05:16 -0300

    feat: establishing connection with mongodb using mongoose and docker

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.set('strictQuery', false);
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
+
+
