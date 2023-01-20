adcac7fd56c373fb0a654904e8e0e714fa24c1bc
João Victor <jvfernandes.contato@gmail.com>
2023-01-20 18:14:50 -0300

    feat: creation of cancelOrder

--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,7 @@ import { listProductsByCategory } from './app/useCases/categories/listProductsBy
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrders';
 import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -40,6 +41,4 @@ router.post('/orders', createOrder);
 
 router.patch('/orders/:orderId', changeOrderStatus);
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.delete('/orders/:orderId', cancelOrder);
